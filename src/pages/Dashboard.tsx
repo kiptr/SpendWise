c8b46579845b855a984c22fa399e71a2d50e3a24
kiptr <2011016210014@mhs.ulm.ac.id>
2025-02-14 15:27:18 +0800

    style: add grid layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import { IncomeExpenses } from "../components/IncomeExpenses";
 
 export function Dashboard() {
   return (
-    <div className="">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
       <BalanceCard balance={11500000} savings={3500000} />
       <IncomeExpenses incomes={5000000} expenses={1500000} />
     </div>
