c61833c05c7ff8ff06a248740c826934ac3c98a6
kiptr <2011016210014@mhs.ulm.ac.id>
2025-02-14 15:25:01 +0800

    feat: integrate IncomeExpenses into dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { BalanceCard } from "../components/BalanceCard";
+import { IncomeExpenses } from "../components/IncomeExpenses";
 
 export function Dashboard() {
-  return <BalanceCard balance={11500000} savings={3500000} />;
+  return (
+    <div className="">
+      <BalanceCard balance={11500000} savings={3500000} />
+      <IncomeExpenses incomes={5000000} expenses={1500000} />
+    </div>
+  );
 }
