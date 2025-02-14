f3d9ef8c75ca44f90a4c8f69bca8b42f9618e8a5
kiptr <2011016210014@mhs.ulm.ac.id>
2025-02-14 16:14:27 +0800

    chore: add SavingPlan and PlanType

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface SavingPlan {
+  id: string;
+  name: string;
+  current: number;
+  target: number;
+  type: string;
+}
+
+export interface PlanType {
+  id: string;
+  name: string;
+  bgColor: string;
+  textColor: string;
+  icon: string;
+}
