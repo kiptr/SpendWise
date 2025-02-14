8b8ef0b649e90fbcc45b433cdd7c0dce41f5c8a8
kiptr <2011016210014@mhs.ulm.ac.id>
2025-02-14 16:15:11 +0800

    feat: integrate SavingPlan into dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { BalanceCard } from "../components/BalanceCard";
 import { IncomeExpenses } from "../components/IncomeExpenses";
+import { SavingPlans } from "../components/SavingPlans";
+import { mockSavingPlans } from "../data/mock";
 
 export function Dashboard() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-      <BalanceCard balance={11500000} savings={3500000} />
-      <IncomeExpenses incomes={5000000} expenses={1500000} />
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <BalanceCard balance={11500000} savings={3500000} />
+        <IncomeExpenses incomes={5000000} expenses={1500000} />
+      </div>
+      <SavingPlans plans={mockSavingPlans} />
     </div>
   );
 }
