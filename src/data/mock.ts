18342cc00ad52ad886f33e3492af41086699933d
kiptr <2011016210014@mhs.ulm.ac.id>
2025-02-14 15:59:56 +0800

    chore add SavingPlans mock data

---
+++ src/data/mock.ts
@@ -0,0 +1,25 @@
+import type { SavingPlan } from "../types";
+
+export const mockSavingPlans: SavingPlan[] = [
+  {
+    id: "1",
+    name: "New Setup Vinyl",
+    current: 732000,
+    target: 1200000,
+    type: "vinyl",
+  },
+  {
+    id: "2",
+    name: "Umroh to Mecca",
+    current: 3560000,
+    target: 122140000,
+    type: "travel",
+  },
+  {
+    id: "3",
+    name: "New Iphone",
+    current: 5120000,
+    target: 20000000,
+    type: "gadget",
+  },
+];
