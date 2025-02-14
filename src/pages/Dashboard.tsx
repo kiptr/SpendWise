0ea5d10505aaf400a15305d55cbe14d09c7f44b9
kiptr <2011016210014@mhs.ulm.ac.id>
2025-02-14 16:21:18 +0800

    style: add grid layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ import { mockSavingPlans } from "../data/mock";
 
 export function Dashboard() {
   return (
-    <div>
+    <div className="cols-span-12 lg:col-span-7 grid grid-rows-[auto_auto_1fr] gap-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
         <BalanceCard balance={11500000} savings={3500000} />
         <IncomeExpenses incomes={5000000} expenses={1500000} />
