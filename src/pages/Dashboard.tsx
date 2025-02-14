25aeb27a992ccebb9b5ad5d369f46466614d2f13
kiptr <2011016210014@mhs.ulm.ac.id>
2025-02-14 18:12:57 +0800

    feat: integrate GeneralPayment into dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import { BalanceCard } from "../components/BalanceCard";
 import { IncomeExpenses } from "../components/IncomeExpenses";
 import { SavingPlans } from "../components/SavingPlans";
-import { mockSavingPlans } from "../data/mock";
+import { GeneralPayment } from "../components/GeneralPayment";
+import { getLast6Months, mockSavingPlans } from "../data/mock";
 
 export function Dashboard() {
+  const monthlyData = getLast6Months();
+
   return (
-    <div className="cols-span-12 lg:col-span-7 grid grid-rows-[auto_auto_1fr] gap-6">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        <BalanceCard balance={11500000} savings={3500000} />
-        <IncomeExpenses incomes={5000000} expenses={1500000} />
+    <div className="grid grid-cols-12 gap-6 flex-1">
+      <div className="cols-span-12 lg:col-span-7 grid grid-rows-[auto_auto_1fr] gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          <BalanceCard balance={11500000} savings={3500000} />
+          <IncomeExpenses incomes={5000000} expenses={1500000} />
+        </div>
+        <SavingPlans plans={mockSavingPlans} />
       </div>
-      <SavingPlans plans={mockSavingPlans} />
+      <div className="col-span-12 lg:col-span-5 grid grid-rows-[320px_1fr] gap-6">
+        <GeneralPayment data={monthlyData} />
+      </div>
     </div>
   );
 }
